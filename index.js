893ac0096e96771b3b827bf994f3ad63190d8ba4
jackyuan256 <jackyuan256@163.com>
2021-04-09 23:10:55 +0800

    refactor index.js & create db.js

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-const homedir = require('os').homedir();
-const home = process.env.HOME || homedir;
-const p = require('path');
-const fs = require('fs');
-const dbPath = p.join(home, '.todo')
+const db = require("./db.js");
 
-module.exports.add = (title) => {
+module.exports.add = async (title) => {
     // 读取之前的task
-    fs.readFile(dbPath, {flag: 'a+'}, (error, data)=>{
-        if(error) {
-            console.log(error)
-        } else {
-            let list;
-            try {
-                list = JSON.parse(data.toString());
-            } catch (e) {
-                list = []
-            }
-            const task = {
-                title: title,
-                done: false
-            };
-            list.push(task);
-            const string = JSON.stringify(list);
-            fs.writeFile(dbPath, string+'\n', (e)=>{
-                if(e) {
-                    console.log(e)
-                }
-            })
-        }
-    })
+    const list = await db.read()
     // 往里面添加一个 title task
+    list.push({title, done: false})
     // 存储任务到文件
+    await db.write(list)
 }

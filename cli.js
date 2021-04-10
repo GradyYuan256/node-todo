df0b4bd6abbc3d9e1e570db576088370905ff72b
jackyuan256 <jackyuan256@gmail.com>
2021-04-10 20:57:43 +0800

    add 'showAll' function

--- cli.js
+++ cli.js
@@ -9,14 +9,18 @@ program
     .description('add a task')
     .action((...args) => {
         const words = args.slice(0, -1).join(' ');
-        api.add(words);
+        api.add(words).then(() => console.log('添加成功'), () => console.log('添加失败'));
     });
 program
     .command('clear')
     .description('clear all tasks')
     .action(() => {
-        api.clear()
+        api.clear().then(() => console.log('清除完毕'), () => console.log('清除失败'));
     });
 
 program.parse(process.argv);
 
+if(process.argv.length ===2) {
+    // 说明用户只运行 node cli.js 命令
+    void api.showAll();
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db = require("./db.js");
+const inquirer = require('inquirer');
 
 module.exports.add = async (title) => {
     // 读取之前的task
@@ -11,4 +12,76 @@ module.exports.add = async (title) => {
 
 module.exports.clear = async () => {
     await db.write([]);
+}
+
+module.exports.showAll = async () => {
+    console.log('show all')
+    // 读取之前的任务
+    const list = await db.read();
+    // 打印之前的任务
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'index',
+            message: '请选择你想操作的任务',
+            choices: [{name: '退出', value: '-1'}, ...list.map((task, index)=>{
+                return {name: `${task.done ? '[x]' : '[_]'} ${index + 1} - ${task.title}`, value: index.toString()}
+            }), {name: '+ 创建任务', value: '-2'}]
+        })
+        .then((answers) => {
+            const index = parseInt(answers.index);
+            if(answers.index >= 0) {
+                // 选择其中一个任务
+                inquirer.prompt({
+                    type: 'list', name: 'action',
+                    message: '请选择操作',
+                    choices: [
+                        {name: '退出', value: 'quit'},
+                        {name: '已完成', value: 'markAsDone'},
+                        {name: '未完成', value: 'markAsUndone'},
+                        {name: '改标题', value: 'updateTitle'},
+                        {name: '删除', value: 'remove'},
+                    ]
+                }).then(answer2 => {
+                    switch (answer2.action) {
+                        case 'markAsDone':
+                            list[index].done = true;
+                            db.write(list);
+                            break;
+                        case 'markAsUndone':
+                            list[index].done = false;
+                            db.write(list);
+                            break;
+                        case 'updateTitle':
+                            inquirer.prompt({
+                                type: 'input',
+                                name: 'title',
+                                message: "新的标题",
+                                default: list[index].title
+                            }).then((answer) => {
+                                list[index].title = answer.title;
+                                db.write(list);
+                            });
+                            break;
+                        case 'remove':
+                            list.splice(index, 1);
+                            db.write(list);
+                            break;
+                    }
+                })
+            } else if(answers.index === -2) {
+                // 创建任务
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'title',
+                    message: '输入任务标题'
+                }).then(answer => {
+                    list.push({
+                        title: answer.title,
+                        done: false
+                    })
+                    db.write(list);
+                })
+            }
+        });
 }

805e2c7487a9367877a5d1c115a3ad84499e7710
jackyuan256 <jackyuan256@gmail.com>
2021-04-11 22:32:03 +0800

    publish on npm & add README.md

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
+#!/usr/bin/env node
 const program = require('commander');
-const api = require('./index.js')
-// const { Command } = require('commander');
-// const program = new Command();
+const api = require('./index.js');
+const pkg = require('./package.json');
 program
-    .option('-x, --xxx', 'What is the x')
+    .version(pkg.version)
 program
     .command('add')
     .description('add a task')
